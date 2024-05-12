ee83e88ca4ef76ca0ae6618eaeab97d88b6d892a
lhf3 <191663059@qq.com>
2024-05-12 14:21:35 +0800

    update：将优惠抽象接口，方便后续扩展

--- src/main/java/com/service/AppleService.java
+++ src/main/java/com/service/AppleService.java
@@ -1,6 +1,5 @@
 package com.service;
 
-import com.oracle.tools.packager.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
