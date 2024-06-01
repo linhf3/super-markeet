4b575d736dac7cead5778796f5a169681c33c056
lhf3 <191663059@qq.com>
2024-06-01 11:46:25 +0800

    Revert "update"
    This reverts commit 18e4f21c

--- src/main/java/com/controller/SalesStatisticalPrice.java
+++ src/main/java/com/controller/SalesStatisticalPrice.java
@@ -1,10 +1,4 @@
 package com.controller;
-
-import com.service.CalculatedAmount;
-import com.service.PreferentialStrategy;
-import com.service.PricingStrategy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +10,6 @@ import java.util.Map;
 @RequestMapping("/sales")
 public class SalesStatisticalPrice {
 
-    @Autowired
-    private PricingStrategy pricingStrategy;
-
-    @Autowired
-    private PreferentialStrategy preferentialStrategy;
-
     /**
      * 统计价格
      *
@@ -30,17 +18,7 @@ public class SalesStatisticalPrice {
      */
     @PostMapping("/statisticalPrice")
     public double statisticalPrice(@RequestBody Map<String, Double> requestMap) {
-        double result = 0d;
-        if (CollectionUtils.isEmpty(requestMap)) {
-            return result;
-        }
-        //1、计算买的水果总价格：requestMap:key为beanName，value为购买数量
-        for (Map.Entry<String, Double> entry : requestMap.entrySet()) {
-            CalculatedAmount calculatedAmount = pricingStrategy.getPriceDiscount(entry.getKey());
-            result += calculatedAmount.calculated(entry.getValue());
-        }
-        //2、是否开启满多少减多少优惠，根据购买情况优惠力度可能不一样
-        return preferentialStrategy.getFavorable("preferentialReliefService").calculated(result);
+        return 0.00;
     }
 
 }
